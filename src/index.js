fa84d6b58a452b080586cba0babb16e444b816b8
David-shankar <davidshankarbalram@gmail.com>
2023-10-13 08:23:26 +0000

    five

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-     <App />
-     </React.StrictMode>,
-   </BrowserRouter>
-),
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 )
-
-   
